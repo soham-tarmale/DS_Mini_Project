9335bd05622e45e0ee0cde85a246f380a279ccfe
Soham-Tarmale-285 <tarmalesoham285@gmail.com>
2026-04-13 17:23:33 +0530

    25102A0028: Made LL structure and initialized main function of Mini_Project

--- Library_Management.c
+++ Library_Management.c
@@ -9,3 +9,16 @@ typedef struct book
     char author[50];
     struct book *next;
 }book;
+
+typedef struct library
+{
+    book *start;
+}library;
+
+int main()
+{
+    int ch, id;
+    char name[50], author[50];
+    library l;
+    l.start = NULL;
+}
